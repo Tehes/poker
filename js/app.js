faca947758eaf5ff19cc78214b944ead8938f946
Tino Sabetta <Tehes@users.noreply.github.com>
2022-04-28 22:19:16 +0200

    can rotate every seat now

--- js/app.js
+++ js/app.js
@@ -5,14 +5,24 @@ Imports
 /* --------------------------------------------------------------------------------------------------
 Variables
 ---------------------------------------------------------------------------------------------------*/
-
+var rotateIcons = document.querySelectorAll(".seat .rotate");
 
 /* --------------------------------------------------------------------------------------------------
 functions
 ---------------------------------------------------------------------------------------------------*/
+function rotateSeat() {
+	var seat = event.target.parentElement.parentElement;
+	seat.dataset.rotation = parseInt(seat.dataset.rotation)+90;
+	seat.style.transform = "rotate("+ seat.dataset.rotation +"deg)";
+}
+
 
 function init() {
     document.addEventListener("touchstart", function() {}, false);
+	
+	for (var rotateIcon of rotateIcons) {
+		rotateIcon.addEventListener("click", rotateSeat, false);
+	}
 }
 
 /* --------------------------------------------------------------------------------------------------
