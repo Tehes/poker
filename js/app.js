6e9d669d6ac2be11c0ffc00dcdc9c8938ae9c9e6
Tino Sabetta <Tehes@users.noreply.github.com>
2022-04-29 22:20:51 +0200

    working on start button

--- js/app.js
+++ js/app.js
@@ -5,11 +5,23 @@ Imports
 /* --------------------------------------------------------------------------------------------------
 Variables
 ---------------------------------------------------------------------------------------------------*/
+var startButton = document.querySelector("aside button");
 var rotateIcons = document.querySelectorAll(".seat .rotate");
+var nameBadges = document.querySelectorAll("h3");
 
 /* --------------------------------------------------------------------------------------------------
 functions
 ---------------------------------------------------------------------------------------------------*/
+function startGame() {
+	for (var rotateIcon of rotateIcons) {
+		rotateIcon.classList.add("hidden");
+	}
+	for (var name of nameBadges) {
+		name.contentEditable = "false";
+	}
+	event.target.classList.add("hidden");
+}
+
 function rotateSeat() {
 	var seat = event.target.parentElement.parentElement;
 	seat.dataset.rotation = parseInt(seat.dataset.rotation)+90;
@@ -19,6 +31,7 @@ function rotateSeat() {
 
 function init() {
     document.addEventListener("touchstart", function() {}, false);
+	startButton.addEventListener("click", startGame, false);
 	
 	for (var rotateIcon of rotateIcons) {
 		rotateIcon.addEventListener("click", rotateSeat, false);
