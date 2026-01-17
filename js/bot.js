3dd52dfc36b29c0f13bf4fd7c185f0a75f7a07c2
Tino Sabetta <tino.sabetta@gmail.com>
2026-01-17 14:22:42 +0100

    feat(styles): adjust hole cards layout and footer padding

--- js/bot.js
+++ js/bot.js
@@ -12,7 +12,7 @@ import { Card, Hand } from "./pokersolver.js";
 ========================== */
 // Configuration constants
 // Delay in milliseconds between enqueued bot actions
-export const BOT_ACTION_DELAY = 1500;
+export const BOT_ACTION_DELAY = 2000;
 
 // Enable verbose logging of bot decisions
 const DEBUG_DECISIONS = false;
