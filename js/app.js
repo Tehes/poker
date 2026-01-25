579b36615a9f7d2c886405a0fa5d90d2d80f7186
Tino Sabetta <tino.sabetta@gmail.com>
2026-01-25 17:31:00 +0100

    feat(bot): update win probability display positioning and styling

--- js/app.js
+++ js/app.js
@@ -1579,7 +1579,7 @@ poker.init();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2026-01-25-v6";
+const SERVICE_WORKER_VERSION = "2026-01-25-v7";
 const AUTO_RELOAD_ON_SW_UPDATE = true;
 
 /* --------------------------------------------------------------------------------------------------
